5bf5fd37f66f3d04382ca497ffe36eef9bee196a
devbewill <sedprc@gmail.com>
2023-12-15 15:16:32 +0100

    added comments

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-
 import { MDXRemote } from "next-mdx-remote/rsc";
+
+/* By default, dynamic routes are generated on-demand at request time which leads to slow loading pages with bad SEO. Instead, as our blogs aren't going to regularly change, we can statically generate the routes for these blogs at build time.
+To do this, we fetch all files in the "blogs" directory and return an array of slugs for each file. */
 
 export async function generateStaticParams() {
   const files = fs.readdirSync(path.join("posts"));
@@ -13,6 +15,8 @@ export async function generateStaticParams() {
 
   return paths;
 }
+
+/* This method will simply fetch a blog post from a given slug. To do this it reads the file in the "blogs" directory with the same filename as the slug. It then uses matter to fetch the metadata and markdown content of this file and returns the FrontMatter metadata, inputted slug and markdown content. */
 
 function getPost({ slug }: { slug: string }) {
   const markdownFile = fs.readFileSync(
@@ -29,6 +33,9 @@ function getPost({ slug }: { slug: string }) {
   };
 }
 
+/* display the blog post itself. We first fetch the post with the getPost method and then display it using the MDXRemote component.
+We also display the blog's title at the top of the page too. */
+
 export default function Post({ params }: any) {
   const props = getPost(params);
 
@@ -40,4 +47,14 @@ export default function Post({ params }: any) {
       <MDXRemote source={props.content} />
     </article>
   );
+}
+
+//METADATA
+export async function generateMetadata({ params }: any) {
+  const blog = getPost(params);
+
+  return {
+    title: blog.frontMatter.title,
+    description: blog.frontMatter.description,
+  };
 }
