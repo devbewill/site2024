d4d2fab132ce7e8f46e5daa3cc97f60238f7b194
Stefano Perelli <sedprc@gmail.com>
2025-02-05 10:08:41 +0100

    Update page.js - removed agenda section from homepage
    removed agenda section

--- src/app/page.js
+++ src/app/page.js
@@ -14,21 +14,19 @@ export default function Home() {
       <main className="w-full lg:grid lg:grid-cols-10 mt-36">
         <Sidebar></Sidebar>
         <div className="mt-20 lg:mt-10 lg:col-start-5 lg:col-span-full">
-          <Section title="Weekly agenda">
-            <WeeklyAgenda></WeeklyAgenda>
-          </Section>
-          <Section title="Last posts">
-            <Lastposts></Lastposts>
-          </Section>
-          <Section title="Last projects">
-            <LastProjects></LastProjects>
-          </Section>
-          <Section title="Experience">
+              <Section title="Experience">
             <Experience></Experience>
           </Section>
           <Section title="Daily tools">
             <Stack></Stack>
           </Section>
+              <Section title="Last projects">
+            <LastProjects></LastProjects>
+          </Section>
+          <Section title="Last posts">
+            <Lastposts></Lastposts>
+          </Section>
+
         </div>
       </main>
     </>
