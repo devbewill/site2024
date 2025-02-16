c322c2b5ecedd04a85688a59e8ff87425a0630b5
Ste Perelli <sedprc@gmail.com>
2025-02-16 20:26:50 +0100

    changed year in footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className="flex justify-between mt-20 py-5 border-t">
-      <p>2023 - Stefano Perelli</p>
+      <p>2025 - Stefano Perelli</p>
       <div>
         <Link href="/" className="transition hover:text-primary">
           Home
